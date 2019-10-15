28431b650ef81469fe3cb6db26980c4b8bba39ee
tedu <1009795943@qq.com>
2019-10-15 10:58:31 +0800

    update

--- home/__init__.py
+++ home/__init__.py
@@ -1,3 +1,4 @@
 from flask import Blueprint
-home=Blueprint('home',__name__,static_folder='static/home',template_folder='templates/home')
+home=Blueprint('home',__name__,
+               template_folder='../templates/home')
 from home import view
