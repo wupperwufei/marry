69eaba7972763a96458f6f4d03f44e056c769c06
tarena <1247962214@qq.com>
2019-11-11 21:17:42 +0800

    zhao

--- models.py
+++ models.py
@@ -10,6 +10,8 @@ class User(db.Model):
     create_time = db.Column(db.DateTime, default=datetime.now)  # 注册时间
     update_time = db.Column(db.DateTime, default=datetime.now)  # 登录时间
     times = db.Column(db.String(30))  # 登录次数
+    user_info = db.relationship('User_info', backref='user', uselist=False)  # 一对一
+    friend = db.relationship('Friend', backref='user', uselist=False)  # 一对一
 
     def __repr__(self):
         return '<User:%r>' % self.iphone
@@ -19,9 +21,8 @@ class User(db.Model):
 class User_info(db.Model):
     __tablename__ = 'user_info'
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), index=True)  # 会员号，外键关联用户表id
-    user = db.relationship('User', backref='user_info', uselist=False)  # 一对一
     nickname = db.Column(db.String(20), index=True, unique=True)  # 昵称
-    # sex =db.Column(db.String(1)) # 性别
+    sex = db.Column(db.Enum('m', 'n'))  # 性别
     birth = db.Column(db.DateTime)  # 出生日期
     age = db.Column(db.String(2))  # 年龄
     high = db.Column(db.String(3), nullable=False)  # 身高
@@ -35,13 +36,16 @@ class User_info(db.Model):
     wechat = db.Column(db.String(30), unique=True, default='null')  # 微信
     email = db.Column(db.String(50), unique=True, default='null')  # 邮箱
     signature = db.Column(db.Text, default='null')  # 个性签名
+    story = db.relationship('Story', backref='user_info', lazy='dynamic')  # 一对多
+
+    def __repr__(self):
+        return '<User_info:%r>' % self.nickname
 
 
 # 择偶标准
 class Friend(db.Model):
     __tablename__ = 'friend'
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), index=True)  # 会员号，外键关联用户表id
-    user = db.relationship('User', backref='friend', uselist=False)  # 一对一
     f_age = db.Column(db.String(2), default='null')  # 年龄
     f_high = db.Column(db.String(3), default='null')  # 身高
     f_education = db.Column(db.String(2), default='null')  # 学历
@@ -58,7 +62,6 @@ class Story(db.Model):
     desc = db.Column(db.String(100), nullable=False)  # 文章描述
     content = db.Column(db.Text, nullable=False)  # 内容
     author = db.Column(db.Integer, db.ForeignKey('user_info.nickname'), index=True)  # 作者，外键关联用户表信息表nickname
-    user_info = db.relationship('User_info', backref='story', lazy='dynamic')  # 一对多
     image = db.Column(db.String(255))  # 图片路径
     created_time = db.Column(db.DateTime, default=datetime.now)  # 发布时间
     times = db.Column(db.String(20), default=0)  # 浏览次数
